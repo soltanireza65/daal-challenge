0a428dc15b0daf7073c27ce69834c9ed3bffed87
Reza Soltani <soltanireza65@gmail.com>
2024-02-02 15:48:26 +0330

    export consts

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,2 +1,6 @@
 export * from './rmq/rmq.module';
 export * from './rmq/rmq.service';
+export * from './events/audit.event';
+export * from './constants/services';
+export * from './constants/events';
+export * from './constants/actions';
