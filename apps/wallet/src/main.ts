4caa7ecd25891120313d77de6413dada96880cab
Reza Soltani <soltanireza65@gmail.com>
2024-02-02 16:09:37 +0330

    add global pipe

--- apps/wallet/src/main.ts
+++ apps/wallet/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { WalletModule } from './wallet.module';
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(WalletModule);
 
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
 
   const configService = app.get(ConfigService)
 
