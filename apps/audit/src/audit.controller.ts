78c54653f1fa9f9691eea85da8217db250d70653
Reza Soltani <soltanireza65@gmail.com>
2024-02-02 20:10:13 +0330

    apply transforms

--- apps/audit/src/audit.controller.ts
+++ apps/audit/src/audit.controller.ts
@@ -3,12 +3,15 @@ import { AuditService } from './audit.service';
 import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
 import { AuditLogEventData } from '@app/common';
 import { EVENT_WALLET_TOPUP } from '@app/common';
+import { Serialize } from '@app/common/interceptors/serializer.interceptor';
+import { AuditDto } from './dtos/audit.dto';
 
 @Controller("audit")
 export class AuditController {
   constructor(private readonly auditService: AuditService) { }
 
   @Get()
+  @Serialize(AuditDto)
   async getAuditLogs() {
     return await this.auditService.getAuditLogs()
   }
--- apps/wallet/src/wallet.controller.ts
+++ apps/wallet/src/wallet.controller.ts
@@ -1,12 +1,15 @@
 import { Body, Controller, Get, Param, Post, UseInterceptors } from '@nestjs/common';
 import { WalletService } from './wallet.service';
 import { UpdateBalanceDTO } from './dto/update-balance.dto';
+import { WalletDto } from './dto/wallet.dto';
+import { Serialize } from '@app/common/interceptors/serializer.interceptor';
 
 @Controller("wallet")
 export class WalletController {
   constructor(private readonly walletService: WalletService) { }
 
   @Get("balance/:user_id")
+  @Serialize(WalletDto)
   getUserBalance(@Param('user_id') user_id: string) {
     return this.walletService.getUserBalance(+user_id);
   }
