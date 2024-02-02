2d8fada8d3af36779d285ec7cee51508f4515a79
Reza Soltani <soltanireza65@gmail.com>
2024-02-02 20:08:24 +0330

    global validation -> audit

--- apps/audit/src/main.ts
+++ apps/audit/src/main.ts
@@ -2,10 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AuditModule } from './audit.module';
 import { RmqService } from '@app/common';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuditModule);
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   const rmqService = app.get<RmqService>(RmqService);
   app.connectMicroservice(rmqService.getOptions('AUDIT'));
   await app.startAllMicroservices();
